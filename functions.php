3e5fcb0fc6ff14a192124cbb9f5e6451cb983a3d
Daniel Olson <emaildano@gmail.com>
2020-07-31 15:43:02 -0400

    added function to sync menus and locations

--- functions.php
+++ functions.php
@@ -1,16 +1,39 @@
 <?php
+
 /**
  * Headless Theme for Shifter functions and definitions
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://developer.wordcodess.org/themes/basics/theme-functions/
  *
  * @package WordPress
  * @subpackage Headless_Theme
  * @since 0.1
  */
 
-function headless_theme_for_shifter_theme_support() {
-	add_theme_support( 'post-thumbnails' );
+function headless_theme_for_shifter_theme_support()
+{
+	add_theme_support('post-thumbnails');
+	add_theme_support('menus');
 }
 
-add_action( 'after_setup_theme', 'headless_theme_for_shifter_theme_support' );
+add_action('after_setup_theme', 'headless_theme_for_shifter_theme_support');
+
+/**
+ * Assign Nav Menu Locations
+ * 
+ * Creates nav menu locations from
+ * registered nav menus and assigns them.
+ */
+function assign_nav_menu_location(string $slug = '', int $term_id = 0, string $name)
+{
+	$locations = get_theme_mod('nav_menu_locations');
+	$locations[$slug] = $term_id;
+	register_nav_menu($slug, __($name, 'headless-theme'));
+	set_theme_mod('nav_menu_locations', $locations);
+}
+
+array_map(
+	fn ($menu) =>
+	assign_nav_menu_location($menu->slug, $menu->term_id, $menu->name),
+	wp_get_nav_menus()
+);
