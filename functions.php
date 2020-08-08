216aba61e302e78d005a66cacd533894b2589de8
Daniel Olson <emaildano@gmail.com>
2020-08-08 17:56:49 -0400

    typo

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /**
  * Headless Theme for Shifter functions and definitions
  *
- * @link https://developer.wordcodess.org/themes/basics/theme-functions/
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package WordPress
  * @subpackage Headless_Theme
