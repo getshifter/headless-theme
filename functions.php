c43eae5b398b03e67a36acc8a6c8d88fd81fb931
Daniel Olson <emaildano@gmail.com>
2020-10-26 20:30:28 -0400

    Removed unused function

--- functions.php
+++ functions.php
@@ -17,23 +17,3 @@ function headless_theme_for_shifter_theme_support()
 }
 
 add_action('after_setup_theme', 'headless_theme_for_shifter_theme_support');
-
-/**
- * Assign Nav Menu Locations
- * 
- * Creates nav menu locations from
- * registered nav menus and assigns them.
- */
-function assign_nav_menu_location(string $slug = '', int $term_id = 0, string $name)
-{
-	$locations = get_theme_mod('nav_menu_locations');
-	$locations[$slug] = $term_id;
-	register_nav_menu($slug, __($name, 'headless-theme'));
-	set_theme_mod('nav_menu_locations', $locations);
-}
-
-array_map(
-	fn ($menu) =>
-	assign_nav_menu_location($menu->slug, $menu->term_id, $menu->name),
-	wp_get_nav_menus()
-);
